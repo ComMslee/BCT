aa894853763d9465776424574dfbb1c07f73f2a8
liam-mslee <110142101+liam-mslee@users.noreply.github.com>
2024-04-18 17:03:18 +0900

    23.04.17 16:10 파일저장 경로 변경

--- src/repository/DataLog.py
+++ src/repository/DataLog.py
@@ -9,39 +9,31 @@ class DataLog(QObject):
     __init = False
     __DEBUG__ = False
 
-    __SAVE_DIR = "data"
-    __FILE_CONFIG = "./" + __SAVE_DIR + "/"
+    __SAVE_DIR = os.path.join(".", "data")
 
     def __init__(self, serialNum, devNum):
         super().__init__()
 
+        current_time = datetime.now()
+        formatted_time = current_time.strftime("%y%m%d")
+        self.__SAVE_DIR = os.path.join(self.__SAVE_DIR, formatted_time)
         if os.path.isdir(self.__SAVE_DIR) is False:
             os.mkdir(self.__SAVE_DIR)
 
         self.serialNum = serialNum
         self.devNum = devNum
-
-        current_time = datetime.now()
-        formatted_time = current_time.strftime("%y%m%d.%H%M%S")
-        self.fileName = f"{serialNum}_{devNum}_{formatted_time}.csv"
+        formatted_time = current_time.strftime("%y%m%d_%H%M%S")
+        self.fileName = f"{formatted_time}_{serialNum}_{devNum}.csv"
         self.writeData(["충전상태", "사이클", "전류", "전압", "온도", "오류코드"])
 
     def writeData(self, data):
-        fullDir = self.__FILE_CONFIG + self.fileName
+        fullDir = os.path.join(self.__SAVE_DIR, self.fileName)
         with open(fullDir, "w", newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(data)
 
     def appendData(self, data):
-        fullDir = self.__FILE_CONFIG + self.fileName
+        fullDir = os.path.join(self.__SAVE_DIR, self.fileName)
         with open(fullDir, "a", newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(data)
-
-    # def loadData(self, fileName: str):
-    #     fullDir = self.__FILE_CONFIG + fileName
-    #     if os.path.isfile(fullDir) and os.path.getsize(fullDir) > 0:
-    #         with open(fullDir, "r") as csv_file:
-    #             pass
-    #     else:
-    #         self.saveData(fileName)

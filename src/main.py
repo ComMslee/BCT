dc4aa34de1c8c60f78b6a0009003513d519a0390
liam-mslee <110142101+liam-mslee@users.noreply.github.com>
2024-07-08 10:39:34 +0900

    1.0.2

--- src/main.py
+++ src/main.py
@@ -48,7 +48,7 @@ class MainWindow(QMainWindow, Ui_BCT):
 if __name__ == '__main__':
     app = QApplication()
     window = MainWindow()
-    window.setWindowTitle("BCT_V1.0.1")
+    window.setWindowTitle("BCT_V1.0.2")
 
     window.show()
     app.exec_()
